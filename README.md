e25ad3ea8d926746b898d21370fed96906e09481
uzhivaj <uzhivaj@gmail.com>
2018-12-05 23:59:26 +0000

    new calculator process w/ humour and while loop

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # project1
-new learning example
+# new learning example
 
 #we're going to take this little by little and slowly build a small project
 #hopefully it will work without any problems
 #this is to get used to the essentials of git
+
+#updates to the system will be made soon.
--- main.py
+++ main.py
@@ -1,5 +1,41 @@
-#we're going to start with the basics
-#lets go
+# we're going to start with the basics
+# lets go
 
-print("hello world!")
-print("let's do something fun today")
+# introduction
+
+print("hello friend!")
+print("let's do some math today!")
+print("..")
+print("but only easy stuff please, I'm still learning.")
+
+# main code
+
+condition = 1
+
+while condition < 4:
+
+    a = input("Enter a number: ")
+    b = input("add[+], subtract[-], multiply[*] or divide[/]? :")
+    c = input("And another..: ")
+
+    if b == "+":
+        print(float(a) + float(c))
+    elif b == "-":
+        print(float(a) - float(c))
+    elif b == "*":
+        print(float(a) * float(c))
+    elif b == "/":
+        print(float(a) / float(c))
+    else:
+        condition += 1
+        print("ERROR...restarting..")
+
+print("***********************")
+print("***********************")
+print("I don't think you really get this whole math-thing.")
+print("I'm going to sleep for a while")
+print("Goodnight <3")
+
+exit()
+
+
